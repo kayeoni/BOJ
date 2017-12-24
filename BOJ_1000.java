45708518dd56cc0021bbaae3bbc2c8591873e924
kayeoni <kayeoni.kim@gmail.com>
2017-12-24 11:36:35 +0900

    A + B

---
+++ BOJ_1000.java
@@ -0,0 +1,18 @@
+/*
+*   A + B
+*
+*   https://www.acmicpc.net/problem/1000
+*/
+
+import java.util.*;
+
+public class BOJ_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
