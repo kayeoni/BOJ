faee576d61a2bab4b9bc848f993abaeb7ef45dc7
kayeoni <kayeoni.kim@gmail.com>
2017-12-24 12:01:27 +0900

    Registration

---
+++ BOJ_7287.java
@@ -0,0 +1,11 @@
+/*
+*   Registration
+*
+*   https://www.acmicpc.net/problem/7287
+*/
+
+public class BOJ_7287 {
+    public static void main(String[] args) {
+        System.out.print("4\nabel1802");
+    }
+}
