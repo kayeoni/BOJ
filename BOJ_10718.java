e6cea7b11516432eb2836a7c37b3d703b3ee2f65
kayeoni <kayeoni.kim@gmail.com>
2017-12-24 12:34:20 +0900

    We love kriii

---
+++ BOJ_10718.java
@@ -0,0 +1,11 @@
+/*
+*   We love kriii
+*
+*   https://www.acmicpc.net/problem/10718
+*/
+
+public class BOJ_10718 {
+    public static void main(String[] args) {
+        System.out.print("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
