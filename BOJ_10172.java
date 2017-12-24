680d2d9c278e38aadaebcdcc95814e373b54f461
kayeoni <kayeoni.kim@gmail.com>
2017-12-24 12:27:40 +0900

    Dogs

---
+++ BOJ_10172.java
@@ -0,0 +1,12 @@
+/*
+*   Dogs
+*
+*   https://www.acmicpc.net/problem/10172
+*/
+
+public class BOJ_10172 {
+    public static void main(String[] args) {
+        System.out.print(
+        "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+    }
+}
