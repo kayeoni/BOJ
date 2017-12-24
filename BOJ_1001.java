9a30f5419e50d69862edf23a5059d9212007e66b
kayeoni <kayeoni.kim@gmail.com>
2017-12-24 11:52:16 +0900

    A - B

---
+++ BOJ_1001.java
@@ -0,0 +1,18 @@
+/*
+*   A - B
+*
+*   https://www.acmicpc.net/problem/1001
+*/
+
+import java.util.*;
+
+public class BOJ_1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a - b);
+    }
+}
