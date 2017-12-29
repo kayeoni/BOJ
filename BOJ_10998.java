805f75fdbf3556dac92674b398c4e60b0ebd0ebf
kayeoni <kayeoni.kim@gmail.com>
2017-12-29 22:07:12 +0900

    A * B

---
+++ BOJ_10998.java
@@ -0,0 +1,20 @@
+/*
+ *	A * B
+ *
+ *	https://www.acmicpc.net/problem/10998
+ */
+
+package BOJ;
+import java.util.*;
+
+public class BOJ_10998 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a*b);
+		sc.close();
+	}
+}
