4e5370ba62e33a51b57a7689c3bfdbba2bb341c3
kayeoni <kayeoni.kim@gmail.com>
2017-12-29 22:00:51 +0900

    Output 2

---
+++ BOJ_11719.java
@@ -0,0 +1,18 @@
+/*
+ *	그대로 출력하기 2
+ *  
+ *	https://www.acmicpc.net/problem/11719
+ */
+
+package BOJ;
+
+import java.util.*;
+
+public class BOJ_11719 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext())
+			System.out.println(sc.nextLine());
+		sc.close();
+	}
+}
