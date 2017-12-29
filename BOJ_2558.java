0489427d628f972cb59af3bb7088b3de84fa18ae
kayeoni <kayeoni.kim@gmail.com>
2017-12-30 01:08:26 +0900

    A + B 2

---
+++ BOJ_2558.java
@@ -0,0 +1,18 @@
+/*
+ * 	A + B 2
+ * 
+ * 	https://www.acmicpc.net/problem/2558
+ */
+
+package BOJ;
+import java.util.*;
+
+public class BOJ_2558 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a + b);
+		sc.close();
+	}
+}
