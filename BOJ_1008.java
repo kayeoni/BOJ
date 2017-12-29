89c2e16f9e78256acb2652495aaced6ab502463d
kayeoni <kayeoni.kim@gmail.com>
2017-12-29 22:15:38 +0900

    A / B

---
+++ BOJ_1008.java
@@ -0,0 +1,18 @@
+/*
+ * 	A / B
+ * 
+ * 	https://www.acmicpc.net/problem/1008
+ */
+
+package BOJ;
+import java.util.*;
+
+public class BOJ_1008 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		System.out.println(a/b);
+		sc.close();
+	}
+}
