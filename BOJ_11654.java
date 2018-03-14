57cf0ec79c992951c4575785732143598d91a031
kayeoni <kayeoni.kim@gmail.com>
2018-03-15 03:24:23 +0900

    ASCII CODE

---
+++ BOJ_11654.java
@@ -0,0 +1,17 @@
+/*
+ *  ASCII CODE
+ *
+ *  https://www.acmicpc.net/problem/11654
+ */
+
+import java.util.*;
+
+public class BOJ_11654 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        char chr = sc.next().charAt(0);
+        sc.close();
+        System.out.println((int) chr);
+    }
+}
+
